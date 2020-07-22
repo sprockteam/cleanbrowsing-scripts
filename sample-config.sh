8511d91ca556de29300a9d4db46ff3ee61091f78
Klint Van Tassel <developer@sprocktech.com>
2020-07-22 09:06:09 -0500

    Updated sample config

--- sample-config.sh
+++ sample-config.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-export CLEANBROWSING_API_URL="https://my.cleanbrowsing.org/api"
+# Enable command tracing if needed for debugging.
+#set -o xtrace
+
+# Set your real API key here.
 export CLEANBROWSING_API_KEY="your-api-key-here"
+
+# Define the profiles you want to manage in this array.
 export CLEANBROWSING_PROFILES=( "default" )
+
+# Don't change this.
+export CLEANBROWSING_API_URL="https://my.cleanbrowsing.org/api"
